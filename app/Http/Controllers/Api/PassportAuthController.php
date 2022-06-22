d3c171a183156c23aa306c4f382a1365a0d482de
ibrahim <ibrahimwali4u@gmail.com>
2022-06-22 12:05:50 +0100

    routing fix

---
+++ app/Http/Controllers/Api/PassportAuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+class PassportAuthController extends Controller
+{
+    //
+    public function register(Request $request){
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('Laravel8PassportAuth')->accessToken;
+  
+        return response()->json(['token' => $token], 200);
+    }
+
+    /**
+     * Login Req
+     */
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+  
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('Laravel8PassportAuth')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+ 
+    public function userInfo() 
+    {
+ 
+     $user = auth()->user();
+      
+     return response()->json(['user' => $user], 200);
+ 
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PassportAuthController;
+use App\Http\Controllers\API\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+  
+Route::middleware('auth:api')->group(function () {
+    Route::get('get-user', [PassportAuthController::class, 'userInfo']);
+ 
+ Route::resource('products', ProductController::class);
+ 
 });
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
