9b3f19f25033afc5614dae734e06ae5af90b4393
ibrahim <ibrahimwali4u@gmail.com>
2022-07-12 12:42:39 +0100

    c

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,6 @@ Route::middleware('auth:api')->group(function () {
  
 });
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
